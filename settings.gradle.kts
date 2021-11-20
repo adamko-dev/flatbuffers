607a7e8aa4088e0931d0d49bb8809483335c7aa4
Adam Semenenko <adam.semenenko@gmail.com>
2021-11-20 13:33:50 +0100

    tidy up android app sub-project, but don't include it as it needs work

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,10 @@ apply(from = "./gradle/shared-build-config/dependency-management.settings.gradle
 include(
   ":language-ports:java",
   ":language-ports:java-grpc",
-  
+
   ":language-ports:kotlin",
 )
+
+// the Android app can be included - but its config is a little outdated
+// and doesn't work on Windows ("CMake Error ... add_library ... Invalid character escape '\U'.")
+//includeBuild("./android")
