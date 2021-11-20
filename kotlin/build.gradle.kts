5cb6c34cabdbb073b424504a22bd4fc18d026dc8
Adam Semenenko <adam.semenenko@gmail.com>
2021-11-20 10:45:00 +0100

    a bit of prep before moving kotlin project

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -5,21 +5,6 @@ plugins {
 group = "com.google.flatbuffers"
 version = "2.0.0-SNAPSHOT"
 
-subprojects {
-
-  repositories {
-    maven { setUrl("https://plugins.gradle.org/m2/") }
-    mavenCentral()
-  }
-}
-
-buildscript {
-  repositories {
-    maven { setUrl("https://plugins.gradle.org/m2/") }
-    gradlePluginPortal()
-    mavenCentral()
-  }
-}
 
 // plugin used to enforce code style
 spotless {
@@ -30,6 +15,22 @@ spotless {
     trimTrailingWhitespace()
     indentWithSpaces()
     endWithNewline()
+    /*
+ * Copyright $YEAR Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
     licenseHeaderFile("$rootDir/spotless/spotless.kt").updateYearWithLatest(false)
     targetExclude("**/spotless.kt", "**/build/**")
   }
