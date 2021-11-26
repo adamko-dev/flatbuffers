6b73f0e7d46466bef67746503598ff623e596036
Adam Semenenko <adam.semenenko@gmail.com>
2021-11-26 18:14:56 +0100

    samples & ports - move languages to be per-folder, not all mixed together

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ include(
 
 // the Android app can be included - but its config is a little outdated
 // and doesn't work on Windows ("CMake Error ... add_library ... Invalid character escape '\U'.")
-//includeBuild("./android")
+//includeBuild("./samples/android")
